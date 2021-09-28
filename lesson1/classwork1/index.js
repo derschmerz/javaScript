6bfc87452e3ea3058602993e1d55e4690a016d0c
derschmerz <derschmerz2@gmail.com>
2021-09-28 22:11:21 +0300

    commit3

--- lesson1/classwork1/index.js
+++ lesson1/classwork1/index.js
@@ -1,18 +1,17 @@
 console.log("hello world");
-const year = 2021
+const year = 2021;
 console.log(year);
-const planetName ="earth"
-console.log(planetName)
-const currentUserTime = "12:20"
-console.log(currentUserTime)
-const countArticle = 10
-console.log(countArticle)
-const isCash = true
-console.log(isCash)
-const firstName = "Oleg"
-console.log(firstName)
-const middleName = "Ivanov"
-console.log(middleName)
-const lastName = "Ivanovich"
-console.log(lastName)
-
+const planetName = "earth";
+console.log(planetName);
+const currentUserTime = "12:20";
+console.log(currentUserTime);
+const countArticle = 10;
+console.log(countArticle);
+const isCash = true;
+console.log(isCash);
+const firstName = "Oleg";
+console.log(firstName);
+const middleName = "Ivanov";
+console.log(middleName);
+const lastName = "Ivanovich";
+console.log(lastName);
