e43e40cca09c6a0fe11cd7befa98b244b0c72537
derschmerz <derschmerz2@gmail.com>
2021-10-02 12:27:38 +0300

    classwork3

---
+++ lesson2/lesson3/classwork3.js
@@ -0,0 +1,84 @@
+//STRING
+/* const firstValue = String(5);
+
+console.log(typeof firstValue); */
+
+/* const obj = {
+  foo: 4,
+};
+console.log(typeof String(obj.foo)); //преобразовать в строку
+ */
+
+// NUMBER
+/* let testValue = "12345zxc"
+testValue = Number(testValue) // преобразовать строку в число
+
+console.log(typeof testValue) */
+
+/* const isUser = true
+const isAdmin = false
+
+console.log(Number(isUser), Number(isAdmin)) // преобразовать булин в число
+
+const voidString =""
+
+console.log(Number(voidString)) // пустая строка в видк числа */
+
+// BOOLEAN
+
+/* const value = 1;
+console.log(typeof Boolean(value), Boolean(value)); 
+ */
+// const obj = [1].length;
+// console.log(Boolean(obj));
+
+/* console.log(2 ** 2); */
+
+// унарный плюс
+
+/* const value = "5"
+
+console.log(typeof value, typeof +value, ) */
+
+// БИНАРНЫЙ ПЛЮС
+
+// const sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+/* const stringValue = "5";
+const numberValue = 34;
+
+console.log(+stringValue + numberValue);
+console.log(numberValue + +stringValue); */
+
+/* const firstName = "Alex";
+const lastName = "Shirikov";
+
+console.log(firstName  + " " + lastName);
+console.log(`${firstName} ${lastName}`); */
+
+//Операции сравнеия
+
+// console.log(false == 0);
+
+/* const firstString = "яблоко";
+const secondString = "Яблоко";
+
+console.log(firstString > secondString); */
+
+/* const a = "5";
+const b = 5;
+
+console.log(a == b);
+console.log(a === b); */
+
+// console.log("" - 1 + 0, typeof ("" - 1 + 0));
+// console.log(true + false + "");
+// console.log(6 / "3");
+console.log(null == "\n0\n");
