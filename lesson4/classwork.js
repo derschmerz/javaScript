f8cec63e6ed9bb526e371f87573da2c6725c5efa
derschmerz <derschmerz2@gmail.com>
2021-10-13 17:48:44 +0300

    classwork4

---
+++ lesson4/classwork.js
@@ -0,0 +1,162 @@
+/* if(1) {
+    console.log("TRUE");
+}
+if(0) {
+    console.log("false");
+} */
+
+/* console.log("START");
+const value = -5;
+
+if (value > 0) {
+    console.log("TRUE");
+} else{
+    console.log("FALSE");
+}
+console.log('FINISH'); */
+
+/* console.log('start');
+let value = 0
+
+if (value === 0) {
+    console.log (0, ' =value') 
+} else if (value > 1) {
+    console.log(value, ' > 1');
+} else if (value < 1) {
+    console.log(value, ' < 1');
+} else {
+    console.log(value);
+        }
+console.log("finish"); */
+
+/* const age = 2
+const result = age >=18 ? "доступ разрешен" : 'доступ запрещен'
+console.log(result); */
+
+/* 
+switch ("max"){
+case "ivan":
+console.log("IVAN");
+break;
+case "Alex":
+console.log('alex');
+break;
+case 'max':
+console.log('max');
+break
+default:
+console.log('имени нет в списке');
+}
+console.log('FINISH') */
+
+/* let a = 39;
+let b = 35;
+let c = 40;
+
+if (a > b) {
+  console.log(a);
+} else if (a > c) {
+  console.log(a);
+} else if (b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
+console.log("finish");
+ */
+/* let t = 120;
+if (t < -30) {
+  console.log("Оставайтесь дома!");
+} else if (t < -10) {
+  console.log("Сегодня холодно");
+} else if (t < 5) {
+  console.log("не холодно");
+} else if (t < 15) {
+  console.log("тепло");
+} else if (t < 25) {
+  console.log("очень тепло");
+} else if (t < 30) {
+  console.log("жарео");
+} else {console.log("пекло");} */
+
+/* onsole.log("FINISH");
+
+let userRole = "admin";
+switch (userRole) {
+  case "admin":
+    console.log("это admin");
+    break;
+  case "user":
+    console.log("это юзер");
+    break;
+  case "manader":
+    console.log("это менеджер");
+break
+default:
+    console.log("no info");
+} */
+
+/* const a = 0;
+const b = 1
+
+const result1 = a||b;
+console.log(result1); */
+
+/* const a = 0;
+const b = 1;
+
+const result = a && b;
+console.log(result); */
+
+/* const a = 1;
+const b = 2;
+const c = 3;
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+} 
+/* let i = 1;
+console.log("start");
+while (true) {
+  console.log("loop");
+  if (i > 10) break;
+} */
+/* console.log("finish"); */
+
+/* let i = 0 */
+/* while (i < 10) {
+    console.log(i);
+    i +=1 /* i=i+1 */
+
+/* do {
+    console.log(i);
+} while (i);
+{console.log('end');} */
+
+/* let i=0;
+for(;i <= 3; i++) {console.log(i);} */
+
+/* let i = 1;
+while (true) {
+    console.log("строка");
+    if (i > 10) break;
+} */
+
+/* let i = 1;
+while (i <= 10) {
+  console.log("строка");
+  i += 1;
+} */
+
+// задача 2
+/* let days =0
+let N = 15;
+let M = 5;
+while (N >= M) {
+    N = (N / 2);
+    days  +=1}
+    console.log(days); */
